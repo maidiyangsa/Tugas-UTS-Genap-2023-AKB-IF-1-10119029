e8680e1826cbb74d2b226bba482ce4bd3076361d
Adrian <adrianmaidiyangsa28@gmail.com>
2023-05-26 10:09:33 +0700

    Tugas UTS AKB 10119029

---
+++ app/src/main/java/com/utsakb/adrianmaidiyangsa_10119029/notes/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.utsakb.adrianmaidiyangsa_10119029.notes;
+
+//Nama : Adrian Maidiyangsa
+//NIM : 10119029
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_profile);
+    }
+}
---
+++ app/src/main/java/com/utsakb/adrianmaidiyangsa_10119029/notes/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.utsakb.adrianmaidiyangsa_10119029.notes;
+
+//Nama : Adrian Maidiyangsa
+//NIM : 10119029
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int SPLASH_SCREEN_TIME = 2000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_SCREEN_TIME);
+    }
+}
